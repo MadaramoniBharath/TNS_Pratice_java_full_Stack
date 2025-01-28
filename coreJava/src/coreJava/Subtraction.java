f716bca16f0b5bb2e757e65b07850f89f85a838d
MadaramoniBharath <bharathmadaramoni@gmail.com>
2025-01-28 18:21:01 +0530

    second commit

---
+++ coreJava/src/coreJava/Subtraction.java
@@ -0,0 +1,14 @@
+package coreJava;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=14;
+		int b=7;
+		int sub=a-b;
+		System.out.println("the sub of two digits are :"+sub);
+
+	}
+
+}
