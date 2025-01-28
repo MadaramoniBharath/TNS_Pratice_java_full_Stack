d937b3ce6a009c1cee4142cf2f49f28eee051196
MadaramoniBharath <bharathmadaramoni@gmail.com>
2025-01-28 17:51:43 +0530

    first commit

---
+++ coreJava/src/coreJava/Addition.java
@@ -0,0 +1,15 @@
+package coreJava;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		int sum=20;
+		System.out.println("the additon of two digits are:"+sum);
+
+	}
+
+}
